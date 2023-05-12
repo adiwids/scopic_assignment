b89fa7ee6a8389af450ded14aacab96186fcd413
Adi Widyawan <day4die@gmail.com>
2023-05-13 06:52:54 +0700

    Implement association and validations on Player model.

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,4 +1,14 @@
 class Player < ApplicationRecord
-  PLAYER_POSITION
-  has_many :player_skills
+  enum position: {
+    defender: 'defender',
+    midfielder: 'midfielder',
+    forward: 'forward'
+  }.freeze
+
+  has_many :player_skills, dependent: :destroy
+
+  validates :name, presence: true,
+                   uniqueness: { scope: :position, case_sensitive: false }
+
+  validates :position, presence: true
 end
--- spec/factories/players.rb
+++ spec/factories/players.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :player do
     name      { Faker::Name.name }
-    position  { [''].sample }
+    position  { %w(defender midfielder forward).sample } # refers to Player.positions enum
   end
 end
