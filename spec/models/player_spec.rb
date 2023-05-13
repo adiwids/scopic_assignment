373dcc53a0e720ac68240a350e556a50b36bf535
Adi Widyawan <day4die@gmail.com>
2023-05-13 08:27:32 +0700

    Add unit test examples for skill nested attributes support on Player model.

--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -35,4 +35,46 @@ RSpec.describe Player, type: :model do
       end
     end
   end
+
+  describe '.player_skills' do
+    context 'nested attributes' do
+      let(:skill_attributes) do
+        [
+          FactoryBot.attributes_for(:player_skill).slice(:skill, :value),
+          FactoryBot.attributes_for(:player_skill).slice(:skill, :value)
+        ]
+      end
+
+      context 'on build or create' do
+        let(:player) { FactoryBot.build(:player, player_skills_attributes: skill_attributes) }
+        it { expect(player.player_skills.size).to eql(skill_attributes.size) }
+      end
+
+      context 'on skill updates' do
+        let(:player) { FactoryBot.create(:player, player_skills_attributes: skill_attributes) }
+        let(:skill) { player.player_skills.first }
+
+        it 'updates skill and value' do
+          new_skill_name = PlayerSkill.skills.values.reject { |sname| sname.eql?(skill.skill) }.sample
+          new_skill_value = rand(0..99)
+
+          player.update(player_skills_attributes: [id: skill.id, skill: new_skill_name, value: new_skill_value])
+
+          skill.reload
+          expect(skill.skill).to eql(new_skill_name)
+          expect(skill.value).to eql(new_skill_value)
+        end
+      end
+
+      context 'on skill removal' do
+        let(:player) { FactoryBot.create(:player, player_skills_attributes: skill_attributes) }
+        let(:removed_skill) { player.player_skills.sample }
+        let(:removal_update) { player.update(player_skills_attributes: [id: removed_skill.id, _destroy: true]) }
+
+        it 'removes skill by ID' do
+          expect { removal_update }.to change { player.player_skills.count }.to(skill_attributes.size - 1)
+        end
+      end
+    end
+  end
 end
