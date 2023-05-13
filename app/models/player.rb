defcf014d3379e5de6a4360db58fb3335339a3c9
Adi Widyawan <day4die@gmail.com>
2023-05-13 08:28:03 +0700

    Implement accept nested attributes for player skills.

--- app/models/player.rb
+++ app/models/player.rb
@@ -6,6 +6,7 @@ class Player < ApplicationRecord
   }.freeze
 
   has_many :player_skills, dependent: :destroy
+  accepts_nested_attributes_for :player_skills, allow_destroy: true
 
   validates :name, presence: true,
                    uniqueness: { scope: :position, case_sensitive: false }
