291713500e0b538cdbafddbf751494c5bae29d48
Adi Widyawan <day4die@gmail.com>
2023-05-13 09:32:01 +0700

    Add player and skills factory templates.

--- spec/factories/player_skills.rb
+++ spec/factories/player_skills.rb
@@ -4,5 +4,39 @@ FactoryBot.define do
 
     skill { %w(defense attack speed strength stamina).sample } # refers to PlayerSkill.skills enum
     value { rand(0..99) }
+
+    trait :high_valued do
+      value { rand(80..99) }
+    end
+
+    trait :medium_valued do
+      value { rand(51..79) }
+    end
+
+    trait :low_valued do
+      value { rand(0..50) }
+    end
+
+    %w(defense attack speed strength stamina).each do |skill_name|
+      trait skill_name.to_sym do
+        skill { skill_name }
+      end
+
+      # TODO: make dynamic as well
+      trait "high_#{skill_name}".to_sym do
+        skill { skill_name }
+        high_valued
+      end
+
+      trait "medium_#{skill_name}".to_sym do
+        skill { skill_name }
+        medium_valued
+      end
+
+      trait "low_#{skill_name}".to_sym do
+        skill { skill_name }
+        low_valued
+      end
+    end
   end
 end
--- spec/factories/players.rb
+++ spec/factories/players.rb
@@ -2,5 +2,55 @@ FactoryBot.define do
   factory :player do
     name      { Faker::Name.name }
     position  { %w(defender midfielder forward).sample } # refers to Player.positions enum
+
+    trait :tough_defender do
+      position { 'defender' }
+
+      player_skills_attributes {
+        %i[high_defense high_strength medium_stamina medium_speed low_attack].map do |skill_trait|
+          FactoryBot.attributes_for(:player_skill, skill_trait).slice(:skill, :value)
+        end
+      }
+    end
+
+    trait :defensive_midfielder do
+      position { 'midfielder' }
+
+      player_skills_attributes {
+        %i[high_defense high_strength high_stamina low_speed low_attack].map do |skill_trait|
+          FactoryBot.attributes_for(:player_skill, skill_trait).slice(:skill, :value)
+        end
+      }
+    end
+
+    trait :attacking_midfielder do
+      position { 'midfielder' }
+
+      player_skills_attributes {
+        %i[low_defense medium_strength medium_stamina medium_speed medium_attack].map do |skill_trait|
+          FactoryBot.attributes_for(:player_skill, skill_trait).slice(:skill, :value)
+        end
+      }
+    end
+
+    trait :attacker do
+      position { 'forward' }
+
+      player_skills_attributes {
+        %i[low_defense low_strength medium_stamina medium_speed high_attack].map do |skill_trait|
+          FactoryBot.attributes_for(:player_skill, skill_trait).slice(:skill, :value)
+        end
+      }
+    end
+
+    trait :target_man do
+      position { 'forward' }
+
+      player_skills_attributes {
+        %i[low_defense medium_strength medium_stamina low_speed high_attack].map do |skill_trait|
+          FactoryBot.attributes_for(:player_skill, skill_trait).slice(:skill, :value)
+        end
+      }
+    end
   end
 end
