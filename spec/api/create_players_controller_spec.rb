1554c653cc3c09a9953ffe6e8055a43a7aef2922
Adi Widyawan <day4die@gmail.com>
2023-05-13 11:57:13 +0700

    Add API test examples for create player cases.

--- spec/api/create_players_controller_spec.rb
+++ spec/api/create_players_controller_spec.rb
@@ -4,20 +4,189 @@ describe Api::PlayersController, type: :request do
   describe 'POST /api/players' do
     context 'test_should_post' do
       it "Should return post" do
-        post '/api/players', params: { '_json' => 
+        post '/api/players', params: { '_json' =>
           {
             "name" => "tst",
             "position" => "defender",
             "player_skills" => [
               {
                 "skill" => "defense",
-                "value" => 10,    
+                "value" => 10,
               }
             ]
           },
         }
 
         expect(response.status).to be_between(1, 900)
+
+        data = json_response
+        expect(data.keys).to match_array(%w(id name position player_skills))
+        skill_data = data['player_skills'].first
+        expect(skill_data.keys).to match_array(%w(id skill value player_id))
+      end
+    end
+
+    context 'with empty player name' do
+      let(:params) do
+        {
+          "_json" => {
+            "name" => '',
+            "position" => FactoryBot.attributes_for(:player)[:position],
+            "player_skills" => [
+              {
+                "skill" => "defense",
+                "value" => 10,
+              }
+            ]
+          }
+        }
+      end
+
+      it 'returns error response' do
+        post '/api/players', params: params
+
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(json_response['message']).to match(/invalid value for name: \(blank\)/i)
+      end
+    end
+
+    context 'with invalid position' do
+      let(:params) do
+        {
+          "_json" => {
+            "name" => FactoryBot.attributes_for(:player)[:name],
+            "position" => "#{FactoryBot.attributes_for(:player)[:position]}-ex",
+            "player_skills" => [
+              {
+                "skill" => "defense",
+                "value" => 10,
+              }
+            ]
+          }
+        }
+      end
+
+      it 'returns error response' do
+        post '/api/players', params: params
+
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(json_response['message']).to match(/invalid value for position: .+/i)
+      end
+    end
+
+    context 'with empty position' do
+      let(:params) do
+        {
+          "_json" => {
+            "name" => FactoryBot.attributes_for(:player)[:name],
+            "position" => '',
+            "player_skills" => [
+              {
+                "skill" => "defense",
+                "value" => 10,
+              }
+            ]
+          }
+        }
+      end
+
+      it 'returns error response' do
+        post '/api/players', params: params
+
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(json_response['message']).to match(/invalid value for position: \(blank\)/i)
+      end
+    end
+
+    context 'with empty skills' do
+      let(:params) do
+        {
+          "_json" => {
+            "name" => FactoryBot.attributes_for(:player)[:name],
+            "position" => FactoryBot.attributes_for(:player)[:position]
+          }
+        }
+      end
+
+      it 'returns error response' do
+        post '/api/players', params: params
+
+        expect(response).to have_http_status(:bad_request)
+        expect(json_response['message']).to match(/invalid value for player skills: \(empty\)/i)
+      end
+    end
+
+    context 'with duplicate skills' do
+      let(:params) do
+        {
+          "_json" => {
+            "name" => FactoryBot.attributes_for(:player)[:name],
+            "position" => FactoryBot.attributes_for(:player)[:position],
+            "player_skills" => [
+              {
+                "skill" => "defense",
+                "value" => 10,
+              },
+              {
+                "skill" => "defense",
+                "value" => 20,
+              }
+            ]
+          }
+        }
+      end
+
+      it 'returns error response' do
+        post '/api/players', params: params
+
+        expect(response).to have_http_status(:bad_request)
+        expect(json_response['message']).to match(/invalid value for player skills: defense \(duplicate\)/i)
+      end
+    end
+
+    context 'with incorrect skill value' do
+      let(:params) do
+        {
+          "_json" => {
+            "name" => FactoryBot.attributes_for(:player)[:name],
+            "position" => FactoryBot.attributes_for(:player)[:position],
+            "player_skills" => [
+              {
+                "skill" => "defense",
+                "value" => -1,
+              }
+            ]
+          }
+        }
+      end
+
+      it 'returns error response' do
+        post '/api/players', params: params
+
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(json_response['message']).to match(/invalid value for defense: -1/i)
+      end
+    end
+
+    context 'with incorrect parameter' do
+      let(:params) do
+        {
+          "name" => FactoryBot.attributes_for(:player)[:name],
+          "position" => FactoryBot.attributes_for(:player)[:position],
+          "player_skills" => [
+            {
+              "skill" => "defense",
+              "value" => -1,
+            }
+          ]
+        }
+      end
+
+      it 'returns error response' do
+        post '/api/players', params: params
+
+        expect(response).to have_http_status(:bad_request)
+        expect(json_response['message']).to match(/missing parameter: _json/i)
       end
     end
   end
