bc8bb91d4cc2fd0b963df691f9218890f479122c
Adi Widyawan <day4die@gmail.com>
2023-05-15 04:50:13 +0700

    Rename top player query to most skilled player query.

--- app/queries/most_skilled_player_query.rb
+++ app/queries/most_skilled_player_query.rb
@@ -1,4 +1,4 @@
-class TopPlayerQuery
+class MostSkilledPlayerQuery
   attr_reader :query
 
   def self.call(filters = [])
@@ -6,7 +6,7 @@ class TopPlayerQuery
   end
 
   def initialize
-    @query = Player.joins(:player_skills).distinct.order('player_skills.value DESC')
+    @query = Player.joins(:player_skills).distinct.order('players.position ASC, player_skills.skill ASC, player_skills.value DESC')
   end
 
   def fetch(filters = [])
