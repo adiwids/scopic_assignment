49a7a9603e1b1e9f9cdbd08f91a324a692bf614f
Adi Widyawan <day4die@gmail.com>
2023-05-15 04:37:07 +0700

    Implement updated validation error messages on player model.

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,5 +1,5 @@
 class Player < ApplicationRecord
-  enum position: {
+  POSITIONS = {
     defender: 'defender',
     midfielder: 'midfielder',
     forward: 'forward'
@@ -8,8 +8,15 @@ class Player < ApplicationRecord
   has_many :player_skills, dependent: :destroy
   accepts_nested_attributes_for :player_skills, allow_destroy: true
 
-  validates :name, presence: true,
-                   uniqueness: { scope: :position, case_sensitive: false }
+  validates :name, presence: { message: 'Invalid empty value for name' },
+                   uniqueness: {
+                    scope: :position,
+                    case_sensitive: false,
+                    message: -> (object, data) do
+                      "has been used for position: #{object.position}"
+                    end
+                  }
 
-  validates :position, presence: true
+  validates :position, presence: { message: 'Invalid empty value for position' },
+                       inclusion: { in: POSITIONS.values, message: 'Invalid value for position: %{value}' }
 end
